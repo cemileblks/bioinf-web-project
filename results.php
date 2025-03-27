dc5abc3fa8e5be49f5acc6d2ed6e2d27787a199a
B272229-2024 <185006791+B272229-2024@users.noreply.github.com>
2025-03-27 14:32:04 +0000

    added additional filter code

--- results.php
+++ results.php
@@ -4,18 +4,33 @@ session_start();
 
 $protein = $_POST['protein'];
 $taxon = $_POST['taxon'];
-// $limit = $_POST['limit'] ?? 50;
+# https://dev.to/lavary/php-double-question-marks-null-coalescing-operator-explained-49d7
+// limit default set to 10
+$limit = $_POST['limit'] ?? 10;
+
+// Check if the user checked the Filter Sequence Length checkbox
+$use_filter = isset($_POST['use_length_filter']);
+
+// if use filter is set, assign their values to min and max otherwise set them to default
+# https://www.php.net/manual/en/language.operators.comparison.php
+$min_len = $use_filter && isset($_POST['min_len']) ? (int) $_POST['min_len'] : 0;
+$max_len = $use_filter && isset($_POST['max_len']) ? (int) $_POST['max_len'] : 100000;
+
+// Validate: make sure min is not greater than max
+if ($min_len > $max_len) {
+    die("Error!!! Minimum length cannot be greater than maximum length.");
+}
 
 // escape characters that can trick the shell command
-$command = escapeshellcmd("python3 scripts/get_sequences.py \"$protein\" \"$taxon\" 10 web_output");
+$command = escapeshellcmd("python3 scripts/get_sequences.py \"$protein\" \"$taxon\" $limit $min_len $max_len");
 
 // execute via shell and return output as string
 // output here is the output from the python script that is shown in comand line
+// https://www.php.net/manual/en/function.shell-exec.php
 $output = shell_exec($command);
 
 echo "<h3>Sequences fetched for: $protein in $taxon</h3>";
 echo "<pre>$output</pre>";
-echo "<a href='index.php'>Back to Home</a>";
-
+echo "<a href='index.php'>Back to Homepage</a>";
 
 ?>
--- scripts/get_sequences.py
+++ scripts/get_sequences.py
@@ -1,34 +1,61 @@
 #!/usr/bin/python3
 import sys, os
-from Bio import Entrez
+from Bio import Entrez, SeqIO
+from io import StringIO
 
 Entrez.email = os.getenv("ENTREZ_EMAIL")
 Entrez.api_key = os.getenv("ENTREZ_API_KEY")
 
-def get_sequences(protein, taxonomic_group, search_limit=10):
-    """Fetch sequences from NCBI based on protein and taxonomic group."""
-    query = f'{protein} AND "{taxonomic_group}"[Organism]'
+# Function to retrieve sequence info based on user parameters with defaults
+def get_sequences(protein, taxonomic_group, search_limit=10, min_len=0, max_len=100000):
+    """Fetch sequences from NCBI based on protein and taxonomic group, and length of sequence"""
+
+    query = f'{protein} AND "{taxonomic_group}"[Organism]' # Query search
+    print(f"Searching NCBI with:\n{query}\n")
+
+    # Overfetch to allow filtering later (so user can still get 20 even if the first search results don't math their critera)
+    retmax = search_limit * 5
     
     # Perform Entrez search
-    handle = Entrez.esearch(db="protein", term=query, retmax=search_limit)
+    handle = Entrez.esearch(db="protein", term=query, retmax=retmax)
     record = Entrez.read(handle)
     
     print(record) # testing
 
+    # Retrive the IDs of the resutls that got returned
     ids = record["IdList"]
-    if len(ids) == 0:
-        print("No sequences were found!")
-        return
 
-    # Retrieve the fasta info for the proteins found
+    # If there are no sequences found:
+    if len(ids) == 0:
+        print("No sequences were found for your qurery!")
+        return
+    
+    print(f"Found {len(ids)} IDs from NCBI (requesting up to {retmax})") # testing
+
+    # Retrieve the fasta info for all the proteins found
     handle = Entrez.efetch(db="protein", id=ids, rettype="fasta", retmode="text")
 
     # read the fasta sequences as string
     fasta_data = handle.read()
+
+    # Parse sequences and filter sequence length (https://biopython.org/wiki/SeqIO)
+    records = list(SeqIO.parse(StringIO(fasta_data), "fasta"))
+    print(f"\nDownloaded {len(records)} total sequences.")
+    filtered_sequences = [rec for rec in records if min_len <= len(rec.seq) <= max_len]
+    print("Found %i sequences that match filter" % len(filtered_sequences))
+
+    # print(records)
+    # print(fasta_data) # testing
+
+    final_sequences = filtered_sequences[:search_limit]
+    print(f"There are {len(final_sequences)} sequences asked for this search")
+    # print(final_sequences)
+
+    for record in final_sequences:
+        print(f"{record.description}")
+        print(f"{record.seq}")
     
-    print(fasta_data) # testing
-    
-    # # Make directory for output file
+    # Make directory for output file
     # os.makedirs("output", exist_ok=True)
 
     # filename = f"output/query_{sys.argv[4]}.fasta"
@@ -39,15 +66,17 @@ def get_sequences(protein, taxonomic_group, search_limit=10):
     # print(f"Saved to {filename}")
 
 
-# Makes sure the required arguments are suppied
+# Makes sure the required arguments are suppied and gives usage instructions
 if len(sys.argv) < 3:
-    print("Usage: fetch_sequences protein_name taxonomic_group [search_limit]")
+    print("Usage: get_sequences.py protein_name taxonomic_group [search_limit] [min_len] [max_len]")
     sys.exit(-1)
 
-# Assign arguments and the optional limit arg
+# Assign arguments and the optional args
 protein = sys.argv[1]
 taxon = sys.argv[2]
-limit = int(sys.argv[3]) if len(sys.argv) > 3 else 10
+limit = int(sys.argv[3])
+min_len = int(sys.argv[4])
+max_len = int(sys.argv[5])
 
-get_sequences(protein, taxon, limit)
+get_sequences(protein, taxon, limit, min_len, max_len)
 
